16e40d331e7670ec09525ea08d6e91dcec4fe7c0
Faraaz Baig <fazzy0908@yandex.com>
2024-08-25 05:06:58 +0530

    Added project structure and todos

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+mod lexer;
+mod parser;
+mod ast;
+mod analyzer;
+mod codegen;
+
+fn main() {
+    println!("RustScript Compiler");
+    // TODO: Implement compiler pipeline
+    // 1. Read input file
+    // 2. Tokenize (lexer)
+    // 3. Parse (parser)
+    // 4. Analyze (semantic analyzer)
+    // 5. Generate Rust code (code generator)
+    // 6. Output Rust code to file
+}
